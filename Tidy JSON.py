52fe3043d5cb9b679f61aedf9ee7cdca269d4016
Chris <chris.longo@gmail.com>
2011-11-23 19:03:09 -0500

    Init repo.

---
+++ Tidy JSON.py
@@ -0,0 +1,62 @@
+import json
+import re
+import sublime
+import sublime_plugin
+
+
+class TidyJsonCommand(sublime_plugin.TextCommand):
+    def load_settings(self):
+        self.settings = {}
+
+        settings = sublime.load_settings(__name__ + '.sublime-settings')
+
+        for setting in ['indent_width',
+                        'item_seperator',
+                        'dictionary_seperator',
+                        'sort_keys',
+                        'compact']:
+
+            self.settings[setting] = settings.get(setting)
+
+    def parse_json(self, input):
+        text = None
+
+        try:
+            obj = json.loads(input)
+
+            text = json.dumps(
+                obj,
+                indent=self.settings.get('indent_width'),
+                sort_keys=self.settings.get('sort_keys'),
+                separators=(self.settings.get('item_seperator'),
+                            self.settings.get('dictionary_seperator')))
+
+        except ValueError:
+            sublime.error_message('Malformed JSON.')
+
+        return text
+
+    def compact(text):
+        pattern = re.compile(r'\s+')
+        return re.sub(pattern, '', text)
+
+    def filter(self, edit, region):
+        text = self.parse_json(self.view.substr(region).encode('utf-8'))
+
+        if text is not None:
+
+            if self.settings.get('compact'):
+                text = self.compact(text)
+
+            self.view.replace(edit, region, text.decode('utf-8'))
+
+    def run(self, edit):
+        self.load_settings()
+
+        regions = self.view.sel()
+
+        if len(regions) == 1 and regions[0].empty():
+            regions = [sublime.Region(0, self.view.size())]
+
+        for region in regions:
+            self.filter(edit, region)
